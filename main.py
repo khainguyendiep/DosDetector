bbedb2569334363e04bed8ace63adc65c7f7afb1
khainguyen <khainguyendiep1107@gmail.com>
2026-03-03 11:03:05 -0500

    [refactor](main): Fix HOST var and changing some vars

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-import socket
 import asyncio
-HOST = 'localhost'
-PORT = 514
 
-class InitUDPConnection:
+HOST = '0.0.0.0'
+PORT = 514
+class syslog_Protocol(asyncio.DatagramProtocol):
     def connection_made(self, transport):
         self.transport = transport
-        print('UDP server get ready')
+        print('UDP server is ready')
     def datagram_received(self, data, addr):
-        message = data.decode()
-        print(message)
+        print(data.decode())
+        #add more filter here
 
 stop_event = asyncio.Event()
 async def main():
-    print('Initing server...')
+    print('Initing monitor...')
     loop = asyncio.get_running_loop()
     transport, protocol = await loop.create_datagram_endpoint(
-            lambda: InitUDPConnection(),
-            local_addr =(HOST, PORT)
+            syslog_Protocol,
+            local_addr=(HOST, PORT)
     )
     try:
         await stop_event.wait()
     finally:
         transport.close()
+
 asyncio.run(main())
